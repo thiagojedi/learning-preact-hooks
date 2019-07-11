ee5f28947ae21a1e0c07c37125f856e4c6441359
Jedi Thiago <thiagojedi@live.it>
2019-07-11 20:48:15 -0300

    Back to local state as I don't mind the cache time

--- src/components/highlight/index.tsx
+++ src/components/highlight/index.tsx
@@ -1,18 +1,29 @@
 import { h } from '/web_modules/preact.js'
-import { useContentDetails } from '../../hooks/content.js';
+import { useState, useEffect } from '/web_modules/preact/hooks.js'
+
 import { useGlobalState } from '../../store/index.js';
+import { getJson } from '../../helpers/http-helper.js';
 
 
 export const Highlight = () => {
     const [state] = useGlobalState();
     const contentId = state.content.highlightId;
-    
-    const defaultContent = { title: '', overview: '' };
-    const [contentDetails = defaultContent] = useContentDetails(contentId)
 
+    const [contentDetails, setContent] = useState({ title: '', overview: '' });
+    const [isLoading, setLoading] = useState(false);
+    useEffect(() => {
+        if (contentId === undefined)
+            return
+        setLoading(true);
+        getJson('https://api.trakt.tv/movies/' + contentId, { extended: 'full' })
+            .then((r) => {
+                setLoading(false);
+                setContent(r);
+            });
+    }, [contentId]);
     return (
         <div class="highlight">
-            <small>{contentId}</small>
+            <small>{contentId}{isLoading && '-loading...'}</small>
             <h1 class="highlight__title">{contentDetails.title}</h1>
             <p class="highlight__description">{contentDetails.overview}</p>
         </div>
